94fdc32307e23a090d638ea077762ef285e3ac80
Anil Kunwar <anilkunwar318@yahoo.com>
2019-06-27 19:14:36 +0200

    Update multiply_a_column_by_constant.py

--- multiplication_column/multiply_a_column_by_constant.py
+++ multiplication_column/multiply_a_column_by_constant.py
@@ -1,3 +1,9 @@
+#References:
+#https://stackoverflow.com/questions/33768122/python-pandas-dataframe-how-to-multiply-entire-column-with-a-scalar
+#https://towardsdatascience.com/pandas-tips-and-tricks-33bcc8a40bb9
+#https://www.pybloggers.com/2018/11/pandas-read-csv-tutorial/
+#https://realpython.com/python-csv/
+#https://chrisalbon.com/python/data_wrangling/pandas_saving_dataframe_as_csv/
 import pandas as pd
 ################################reading input em variables##############################################
 df=pd.read_csv('./fluid.csv', usecols=[0,1,2,3,4])
