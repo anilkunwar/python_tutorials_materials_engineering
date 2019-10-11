b848220822d6b054b36d33c8607900754a17eaba
Anil Kunwar <anilkunwar318@yahoo.com>
2019-10-11 15:12:15 +0200

    Create fruits_place.py

---
+++ boolean_column_readings_csv_files/select_specific_rows/fruits_place.py
@@ -0,0 +1,20 @@
+#jonathansoma.com/lede/foundations-2017/classes/working-with-many-files/class/
+#thispointer.com
+#marsja.se/pandas-read-csv-tutorial-to-csv/
+import pandas as pd
+import numpy as np
+import glob
+#* means match anything
+# glob can find the full path unlike os, fmatch , for which we need to provide path
+csv_files=glob.glob("date*.csv")
+#Having a list of filenames, we can convert those into list of dataframes
+#Choose only second rows of each file (3rd rows including the headers)
+rows_to_keep=[2]
+#rows_to_keep=[2,3] #multiple rows
+dfs=[pd.read_csv(csv_file, skiprows=lambda x: x not in rows_to_keep) for csv_file in csv_files]
+#df=pd.append(dfs)
+df=pd.concat(dfs,sort=False)
+#df=pd.concat(dfs,sort=True)
+type(dfs)
+print(dfs)
+df.to_csv("MultipleDfs.csv", index=False)
