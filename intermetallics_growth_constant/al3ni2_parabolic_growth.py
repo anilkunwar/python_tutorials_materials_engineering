cbfee50f03a99035f444d863146faf7f59684d79
Anil Kunwar <anilkunwar318@yahoo.com>
2020-05-11 03:31:15 +0200

    Update al3ni2_parabolic_growth.py

--- intermetallics_growth_constant/al3ni2_parabolic_growth.py
+++ intermetallics_growth_constant/al3ni2_parabolic_growth.py
@@ -1,4 +1,4 @@
-Reference: Schoo et al, Defect and Diffusion Forum, 143-147 (2009), pp. 631-636
+#Reference: Schoo et al, Defect and Diffusion Forum, 143-147 (2009), pp. 631-636
 ########################
 import numpy as np
 ######Parabolic growth rate with temperature dependent growth rate constant arrheniusk#######
@@ -9,7 +9,16 @@ def arrheniusk(k0,Qa,R,T):
 def xparabolic(x0,k,t,n):
     pow_term=np.power(t,n)
     return x0+k*pow_term
-# numerical data 
+###############################################
+# numerical data for growth constant calculation and layer thickness
 k0=3.7E-06 #m^2/s
 Qa= 1.26E+05 #J/mol
-R=
+R=8.31 # J/mol K
+T = 800 # K
+x0 = 2.0E-08 # m
+t = 60 # s
+n = 0.5 # diffusion based parabolic growth , no units
+###############calculation of growth constant#############################
+kcalc = arrheniusk(k0,Qa,R,T)
+print("Growth constant (m^2/s) at the temperature=",kcalc)
+
