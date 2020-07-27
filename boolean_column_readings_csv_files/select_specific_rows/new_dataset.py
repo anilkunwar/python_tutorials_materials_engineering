67f5a641699a21f92c973edadfc8959796c71204
Anil Kunwar <anilkunwar318@yahoo.com>
2020-07-27 11:26:37 +0200

    Update new_dataset.py

--- boolean_column_readings_csv_files/select_specific_rows/new_dataset.py
+++ boolean_column_readings_csv_files/select_specific_rows/new_dataset.py
@@ -6,8 +6,8 @@ import pandas as pd
 df=pd.read_csv('./old_dataset.csv') # Columns not mentioned because some rows have no defined columns
 # choose the regime of diffuse interface between t,he phases
 #df1 = df.iloc[num1:num2] # use with care to use rows from middle of total observations
-#df1 = df.iloc[num1:] # Deletes all the preceding rows  to (num1+2)th row
-#df1 = df.iloc[:num2] # Deletes all the rows following (num2+2)th row
+#df1 = df.iloc[num1:] # Deletes all the preceding rows  to (num1+index)th row
+#df1 = df.iloc[:num2] # Deletes all the rows following (num2+index)th row
 df1 = df.iloc[2:] # Extract data after the first two rows + index row
 output1=df1.to_csv('./lower_two_rows_new_dataset.csv', index=False)
 df1 = df.iloc[:2] # Extract data upto the first two rows + index row 
